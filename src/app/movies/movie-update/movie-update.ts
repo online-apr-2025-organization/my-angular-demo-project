326dd34e99db47fe6d00430df1f33186662b5ee7
bpoongs <bpoongs@gmail.com>
2025-06-04 10:48:52 +0530

    reactive form

--- src/app/movies/movie-update/movie-update.ts
+++ src/app/movies/movie-update/movie-update.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Movie } from '../model/movie.model';
 import { MoviesService } from '../services/movies.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'movie-update',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './movie-update.html',
   styleUrl: './movie-update.css',
 })
@@ -20,15 +26,23 @@ export class MovieUpdate {
     // for reactive forms we create the FormControl objects here
     this.myReactiveForm = new FormGroup({
       movieId: new FormControl(this.fetchedMovie.movieId),
-      movieName: new FormControl(this.fetchedMovie.movieName),
+      movieName: new FormControl(this.fetchedMovie.movieName, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(20),
+      ]),
       movieDescription: new FormControl(this.fetchedMovie.movieDescription),
-      movieYear: new FormControl(this.fetchedMovie.movieYear),
+      movieYear: new FormControl(
+        this.fetchedMovie.movieYear,
+        Validators.required
+      ),
       movieImageUrl: new FormControl(this.fetchedMovie.movieImageUrl),
     });
     // after creating the FormGroup of FormControl object, now link these FormGroup and FormControl objects in the template
   }
 
   myFormSubmit() {
+    console.log(this.myReactiveForm);
     console.log(this.myReactiveForm.value);
   }
 }
